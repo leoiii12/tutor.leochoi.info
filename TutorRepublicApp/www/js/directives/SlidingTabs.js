6697f13c70bba558ca42e30291e81b1bfa207eac
Leo Choi <choimankin@gmail.com>
2015-10-06 20:35:44 +0800

    1. Update style 2. Use the lateste sliding tabs

--- TutorRepublicApp/www/js/directives/SlidingTabs.js
+++ TutorRepublicApp/www/js/directives/SlidingTabs.js
@@ -1,12 +1,5 @@
 ﻿trDirectives
-.directive('slidingTabs', [
-  '$timeout',
-  '$compile',
-  '$ionicSlideBoxDelegate',
-  '$ionicHistory',
-  '$ionicScrollDelegate',
-  '$ionicSideMenuDelegate',
-function ($timeout, $compile, $ionicSlideBoxDelegate, $ionicHistory, $ionicScrollDelegate, $ionicSideMenuDelegate) {
+.directive('slidingTabs', ['$timeout', '$compile', '$ionicSlideBoxDelegate', '$ionicScrollDelegate', '$ionicSideMenuDelegate', '$ionicHistory', function ($timeout, $compile, $ionicSlideBoxDelegate, $ionicScrollDelegate, $ionicSideMenuDelegate, $ionicHistory) {
     return {
         restrict: 'E',
         replace: true,
@@ -18,17 +11,18 @@ function ($timeout, $compile, $ionicSlideBoxDelegate, $ionicHistory, $ionicScrol
         },
         controller: ['$scope', '$element', '$attrs', function ($scope, $element, $attrs) {
             var _this = this;
-            $scope.tabLabels = [];
-
+           
             // Disable sidemenu
             $ionicSideMenuDelegate.canDragContent(false);
 
             var slider = new ionic.views.Slider({
                 el: $element[0],
-                startSlide: $scope.activeSlide,
+                auto: 0,
                 continuous: false,
+                startSlide: $scope.activeSlide,
                 slidesChanged: function () {
                     $scope.currentSlide = slider.currentIndex();
+
                     // Try to trigger a digest
                     $timeout(function () { });
                 },
@@ -37,6 +31,7 @@ function ($timeout, $compile, $ionicSlideBoxDelegate, $ionicHistory, $ionicScrol
                     $scope.onSlideChanged({ index: $scope.currentSlide, $index: $scope.currentSlide });
                     $scope.$parent.$broadcast('slideBox.slideChanged', slideIndex);
                     $scope.activeSlide = slideIndex;
+
                     // Try to trigger a digest
                     $timeout(function () { });
                 },
@@ -60,73 +55,86 @@ function ($timeout, $compile, $ionicSlideBoxDelegate, $ionicHistory, $ionicScrol
 
             slider.enableSlide($scope.$eval($attrs.disableScroll) !== true);
 
+            var deregisterInstance = $ionicSlideBoxDelegate._registerInstance(slider, $attrs.delegateHandle, function () {
+                return $ionicHistory.isActiveScope($scope);
+            });
+
             $scope.$watch('activeSlide', function (nv) {
                 if (angular.isDefined(nv)) {
                     slider.slide(nv);
                 }
             });
 
-            var deregisterInstance = $ionicSlideBoxDelegate._registerInstance(
-              slider, $attrs.delegateHandle, function () {
-                  return $ionicHistory.isActiveScope($scope);
-              }
-            );
+            $scope.$on('slideBox.nextSlide', function () {
+                slider.next();
+            });
+
+            $scope.$on('slideBox.prevSlide', function () {
+                slider.prev();
+            });
+
+            $scope.$on('slideBox.setSlide', function (e, index) {
+                slider.slide(index);
+            });
 
             $scope.$on('$destroy', function () {
                 deregisterInstance();
                 slider.kill();
+
+                // Enable sidemenu
+                $ionicSideMenuDelegate.canDragContent(true);
             });
 
             this.selectTab = function (index) {
                 slider.slide(index);
             };
 
+            $scope.tabLabels = [];
             this.addTabLabel = function (tabLabel) {
                 $scope.tabLabels.push(tabLabel);
             };
 
-            this.getHeight = function () {
-                return $element[0].parentElement.clientHeight - 48 -44;
-            };
-
             $timeout(function () {
                 slider.load();
-                $scope.tabCount = slider.count;
             });
         }],
-        template: '<div class="slider">' + '<div class="slider-slides" ng-transclude>' + '</div>' + '</div>',
+        template: '<div style="padding-bottom: 48px;" class="slider">' + '<div class="slider-slides" style="height: 100%;" ng-transclude>' + '</div>' + '</div>',
         link: function ($scope, $element, $attr) {
-            var childScope = $scope.$new();
-            var pager = angular.element('<sliding-tabs-selector></sliding-tabs-selector>');
-            $element.prepend(pager);
-            pager = $compile(pager)(childScope);
+            var selectorScope = $scope.$new();
+            var selector = angular.element('<sliding-tabs-selector></sliding-tabs-selector>');
+            $element.prepend(selector);
+            selector = $compile(selector)(selectorScope);
         }
     };
 }])
-.directive('slidingTabsPage', function () {
+.directive('slidingTabsPage', ['$timeout', function ($timeout) {
     return {
         restrict: 'E',
         require: '^slidingTabs',
-        scope: false,
+        scope: {
+            isPadding: '='
+        },
         transclude: true,
-        template: '<ion-scroll style="height: {{containerHeight}}px" class="padding"><div ng-transclude></div></ion-scroll>',
+        template: '<ion-scroll style="height: 100%;"><div ng-class="{\'padding\' : isPadding}" ng-transclude></div></ion-scroll>',
         link: function ($scope, $element, $attrs, slidingTabs) {
             $element.addClass('slider-slide');
             slidingTabs.addTabLabel($attrs.slidingTabsPageLabel);
-            $scope.containerHeight = slidingTabs.getHeight();
         }
     };
-})
+}])
 .directive('slidingTabsSelector', function () {
     return {
         restrict: 'E',
         require: '^slidingTabs',
-        template: '<sliding-tabs-label ng-repeat="(key, tabLabel) in tabLabels track by key" ng-bind="tabLabel" ng-click="selectTab(key)">' + '</sliding-tabs-label>',
+        template: '<sliding-tabs-label ng-repeat="(key, tabLabel) in tabLabels track by key" ng-bind="tabLabel" ng-click="selectTab(key)"></sliding-tabs-label>',
         link: function ($scope, $element, $attr, slidingTabs) {
+            $scope.selectTab = function (index) {
+                slidingTabs.selectTab(index);
+            };
+
             var selectPage = function (index) {
                 var children = $element[0].children;
-                var length = children.length;
-                for (var i = 0; i < length; i++) {
+                for (var i = 0; i < children.length; i++) {
                     if (i == index) {
                         children[i].classList.add('active');
                     } else {
@@ -135,14 +143,10 @@ function ($timeout, $compile, $ionicSlideBoxDelegate, $ionicHistory, $ionicScrol
                 }
             };
 
-            $scope.selectTab = function (index) {
-                slidingTabs.selectTab(index);
-            };
-
             $scope.$watch('currentSlide', function (v) {
                 selectPage(v);
             });
         }
-
     };
-});
+})
+;
